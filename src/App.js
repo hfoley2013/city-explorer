5cce8427b3051167dfc99231143399f8cbaa5411
Harper Foley <harper.e.foley@gmail.com>
2022-11-14 21:41:42 +0000

    Runs build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
+
+handleSubmit = (e) => {
+  e.preventDefault();
+  // API data
+  
+  //save data to state
+}
+//handleCityInput = (e) => {}
+//handleCitySubmit = (e) => {}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <h1>City Explorer</h1>
+    <form onSumbit={this.handleSubmit}>
+      <button type="submit">Submit City</button>
+    </form>
+  </>
   );
+}
+
+
+
+
 }
 
 export default App;
